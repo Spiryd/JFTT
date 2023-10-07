6f881a992f22c7abb94873a43afac236ff933248
Spiryd <neumann.maks@gmail.com>
2023-10-07 21:38:12 +0200

    implemented fam

--- l1/src/bin/fa.rs
+++ l1/src/bin/fa.rs
@@ -36,22 +36,31 @@ fn extract_input_alphabet(txt: &String) -> HashSet<char> {
 }
 
 fn finite_automaton_matcher(pattern: &String, txt: &String) {
-    let m = pattern.len();
-    let n = txt.len();
-    let tf = compute_transition_function();
+    let m = pattern.chars().collect::<Vec<_>>().len();
+    let n = txt.chars().collect::<Vec<_>>().len();
+    let tf = compute_transition_function(m, extract_input_alphabet(txt), pattern);
+    //dbg!(&tf);
     let mut state = 0;
     for i in 0..n {
-        state = *tf[state].get(&pattern.chars().nth(i).unwrap()).unwrap();
+        state = *tf[state].get(&txt.chars().nth(i).unwrap()).unwrap();
         if state == m {
             println!("Found at {}", i-m+1);
         }
     }
 }
 
-fn next_state() {
-    todo!()
-}
-
-fn compute_transition_function() -> Vec<HashMap<char, usize>>{
-    todo!()
+fn compute_transition_function(m: usize, alphabet: HashSet<char>, pattern: &String) -> Vec<HashMap<char, usize>>{
+    let mut transition_function: Vec<HashMap<char, usize>> = vec![HashMap::new(); m+1];
+    for q in 0..=m {
+        for ch in &alphabet {
+            let mut k = m.min(q+1);
+            while !&format!("{}{}", pattern.chars().take(q).collect::<String>(), ch).ends_with(&pattern.chars().take(k).collect::<String>()){
+                //println!("{:?}", !pattern_starts_with_suffix(&pattern.chars().take(q).collect::<String>(), &pattern.chars().take(k).collect::<String>()));
+                k -= 1;
+            }
+            transition_function[q].insert(*ch, k);
+            //dbg!(&transition_function);
+        }
+    }
+    transition_function
 }
